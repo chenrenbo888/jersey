1bbd1f42245fe11091479ea721f3066da92ab6d1
renboqie <470371045@qq.com>
2016-09-06 00:01:09 +0800

    图片上传
    可以将图片上传到另外一台服务器

---
+++ src/win/renboqieqie/JerseyDemo.java
@@ -0,0 +1,32 @@
+package win.renboqieqie;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+
+/**
+ * 发送图片到另外一台tomcat服务器
+ * @author Administrator
+ *
+ */
+public class JerseyDemo {
+
+
+	public static void main(String[] args) throws IOException {
+		//实例化一个jersey
+		Client client = new Client();
+		//另一台服务器的路径
+		String url = "http://localhost:8088/image-web/upload/qqqqq.jpg";
+		//设置请求的路径
+		WebResource resource = client.resource(url);
+		//读取本地图片为流或二进制
+		String path = "F:\\mypicture\\intro-bg.jpg";
+		byte[] readFileToByteArray = FileUtils.readFileToByteArray(new File(path));
+		 //发送请求
+		resource.put(String.class,readFileToByteArray);
+	}
+}
